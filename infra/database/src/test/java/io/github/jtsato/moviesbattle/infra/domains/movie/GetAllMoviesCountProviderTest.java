d2a39bc912c33d1afe6032edc0ad4fe9e2521b32
jtsato <jorge.takeshi.sato@gmail.com>
2023-03-25 21:38:15 -0300

    test(infra): add unit tests

---
+++ infra/database/src/test/java/io/github/jtsato/moviesbattle/infra/domains/movie/GetAllMoviesCountProviderTest.java
@@ -0,0 +1,42 @@
+package io.github.jtsato.moviesbattle.infra.domains.movie;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Jorge Takeshi Sato
+ */
+
+@DisplayName("Get All Movies Count Provider Test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DataJpaTest
+@Import({GetAllMoviesCountProvider.class})
+@Sql("GetAllMoviesCountProviderTest.sql")
+class GetAllMoviesCountProviderTest {
+
+    @Autowired
+    private GetAllMoviesCountProvider getAllMoviesCountProvider;
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @DisplayName("Get All Movies Count")
+    @Test
+    void successfulToGetAllMoviesCount() {
+        // Arrange
+        // Act
+        final Long actual = getAllMoviesCountProvider.execute();
+
+        // Assert
+        assertThat(actual).isEqualTo(2L);
+
+        assertThat(movieRepository.count()).isEqualTo(2L);
+    }
+}
---
+++ infra/database/src/test/java/io/github/jtsato/moviesbattle/infra/domains/movie/GetMovieByImdbIdProviderTest.java
@@ -0,0 +1,59 @@
+package io.github.jtsato.moviesbattle.infra.domains.movie;
+
+import io.github.jtsato.moviesbattle.core.domains.movie.model.Movie;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Jorge Takeshi Sato
+ */
+
+@DisplayName("Get Movie By ImdbId Provider Test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DataJpaTest
+@Import({GetMovieByImdbIdProvider.class})
+@Sql("GetMovieByImdbIdProviderTest.sql")
+class GetMovieByImdbIdProviderTest {
+
+    @Autowired
+    private GetMovieByImdbIdProvider getMovieByImdbIdProvider;
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @DisplayName("Get Movie By ImdbId")
+    @Test
+    void successfulToGetMovieByImdbId() {
+        // Arrange
+        // Act
+        final Optional<Movie> optional = getMovieByImdbIdProvider.execute("tt0468569");
+
+        // Assert
+        assertThat(optional).isPresent();
+
+        final Movie movie = optional.get();
+
+        assertThat(movie).isNotNull();
+
+        assertThat(movie.id()).isEqualTo(2L);
+        assertThat(movie.imdbId()).isEqualTo("tt0468569");
+        assertThat(movie.title()).isEqualTo("The Dark Knight");
+        assertThat(movie.year()).isEqualTo("2008");
+        assertThat(movie.genre()).isEqualTo("Action, Crime, Drama");
+        assertThat(movie.imdbRating()).isEqualTo(9.0F);
+        assertThat(movie.imdbVotes()).isEqualTo(2628154L);
+        assertThat(movie.score()).isEqualTo(23653386L);
+        assertThat(movie.posterUrl()).isEqualTo("https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg");
+
+        assertThat(movieRepository.count()).isEqualTo(2L);
+    }
+}
